4dd11f406a199f21f94319d4d0855509beffa375
Maurice Manz <manz.maurice-it24@it.dhbw-ravensburg.de>
2025-02-07 09:34:02 +0100

    turned basics.c into arrays.c

---
+++ arrays.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[5];
+    int b[5] = {0};
+    int c[5] = {11, 22, 33, 44, 55};
+    int d[] = {11, 22, 33, 44, 55};
+    printf("%d\n", a[0]);
+    printf("%d\n", b[0]);
+    printf("%d\n", c[0]);
+    printf("%d\n", d[0]);
+    return 0;
+}
--- basics.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("Heöp");
-    return 0;
-
-}
