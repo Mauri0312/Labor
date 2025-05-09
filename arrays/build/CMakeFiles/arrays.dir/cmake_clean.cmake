a4d7c263b148bff106987b3c491f97adee32986f
Mauri0312 <manz.maurice-it24@it.dhbw-ravensburg.de>
2025-05-09 11:56:19 +0200

    c++finished

---
+++ arrays/build/CMakeFiles/arrays.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arrays.dir/link.d"
+  "CMakeFiles/arrays.dir/arrays.cpp.o"
+  "CMakeFiles/arrays.dir/arrays.cpp.o.d"
+  "arrays"
+  "arrays.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arrays.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
